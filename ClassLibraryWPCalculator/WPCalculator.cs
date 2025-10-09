8905429d5140515e491d972d83b39789e0e6cbfc
matveybardack <matbardack@gmail.com>
2025-10-09 14:36:06 +0300

    temp fix

---
+++ ClassLibraryWPCalculator/WPCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryWPCalculator
+{
+    public class WPCalculator
+    {
+        private string post;
+        private string pred;
+
+        public WPCalculator(string pred, string post)
+        { 
+            this.pred = pred;
+            this.post = post;
+        }
+
+        public 
+    }
+}
